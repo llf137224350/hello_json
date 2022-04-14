d3a9c9c95eb36f231ade8243239fd66763a33653
llf137224350 <137224350@qq.com>
2022-04-14 12:14:07 +0800

    chore: 描述调整

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -22,6 +22,7 @@ export default class NavBar extends Component {
             <div className="tip">
               由于部署当前工具需要购买服务器和域名，对于个人开发者来说，并不是件轻松的事情。如果您觉得该工具好用，条件允许的情况下，可以试着赞助一下。谢谢！
             </div>
+            <div className="tip">联系开发者，微信号：llf137224350</div>
             <img
               className="collection-code"
               src="https://hsh.cyzl.com/file-server/g001/M00/01/90/oYYBAGJXiZCAdDCdAAN2ScI5dfM259.jpg"
